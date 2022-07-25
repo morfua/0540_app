2aa81fdb2c62b2c48f36231475f579f6c409a5da
G.Seergey <morfua@gmail.com>
2022-07-25 11:01:13 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Привет</h1>
+      <span>lore</span>
+    </div>
+  );
+}
+
+export default App;
