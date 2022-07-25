712bd9ad4dba89aacf854ed98aa7cea9e1228b99
G.Seergey <morfua@gmail.com>
2022-07-25 12:18:35 +0300

    add routing vs 2 component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-// import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import { NavLink, Routes, Route } from "react-router-dom";
+
+const Profile =()=>{
+  return (
+    <h1>Это страница с профилем</h1>
+  )
+}
+const Messages = () => {
+	return <h1>Это страница с сообщениями</h1>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Привет</h1>
-      <span>lore</span>
-    </div>
-  );
+	return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-sm-3">
+					<div className="nav flex-column nav-pills" aria-orientation="vertical">
+						<NavLink className="nav-link" to="profile">
+							Профиль
+						</NavLink>
+						<NavLink className="nav-link" to="messages">
+							Сообщения
+						</NavLink>
+						<NavLink className="nav-link" to="settings">
+							Настройки
+						</NavLink>
+						<NavLink className="nav-link" to="friends">
+							Друзья
+						</NavLink>
+					</div>
+				</div>
+				<div className="col-sm-9">
+					<Routes>
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/messages" element={<Messages/>} />
+					</Routes>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
